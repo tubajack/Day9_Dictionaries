3eef4f703aef6cdc2b2e595f3da2b8ab6d9d11ed
tubajack <jackson.henry13@yahoo.com>
2025-08-11 11:53:18 -0500

    Added functionality for the bidding.

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from replit import clear
+from helpers import clear_screen
 from art import logo
 print(logo)
 
 auction_bids = {}
 biddingComplete = False
+
+def determineHighestBidder(recordBid):
+    highest_bid = 0
+    champion = ""
+
+    for bidder in recordBid:
+        bid_amount = recordBid[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            champion = bidder
+    print(f"The winner is {champion} with a bid of ${highest_bid}")
 
 while biddingComplete is False:
     name = input("What is your name? ")
@@ -13,4 +24,9 @@ while biddingComplete is False:
     proceed = input("Are there any other bidders? Click 'yes' or 'no'. \n")
 
     if proceed == "yes":
-        clear()
+        clear_screen()
+    elif proceed == "no":
+        biddingComplete = True
+        determineHighestBidder(auction_bids)
+
+
